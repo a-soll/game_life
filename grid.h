d17f72d9e9c8bbdcb41ff5fb26d63359a285bbc0
Adam Solloway <asolloway@worcester.edu>
2020-10-22 20:01:40 -0400

    Neighbor check

--- grid.h
+++ grid.h
@@ -3,21 +3,59 @@
 const int width = 6;
 const int height = 6;
 
-void point(int x, int y, std::string arr[width][height]){
-    arr[x][y] = "O";
+void point(int x, int y, int arr[width][height])
+{
+    arr[x][y] = 0;
 }
 
-void draw(std::string arr[][width]){
-    std::string out;
-    for (int i = 0; i < height; i++){
-        for (int j = 0; j <= width; j++){
-            if (j == width){
-                out.append("\n");
+void draw(int arr[][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j <= width; j++)
+        {
+            if (j == width)
+            {
+                std::cout << '\n';
             }
-            else {
-                out.append(arr[i][j] + " ");
+            else
+            {
+                std::cout << arr[i][j];
+                std::cout << ' ';
             }
         }
     }
-    std::cout << out << std::endl;
+}
+
+void evaluate(int arr[][width])
+{
+    int neighbors = 0;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (size_t j = 0; j < width; j++)
+        {
+            int spot = arr[i][j];
+            if (i != 0 && j != 0)
+            {
+                neighbors += arr[i - 1][j - 1];
+            }
+            if (i != 0)
+            {
+                neighbors += arr[i - 1][j];
+                neighbors += arr[i - 1][j + 1];
+            }
+            if (j != 0)
+            {
+                neighbors += arr[i][j - 1];
+                neighbors += arr[i + 1][j - 1];
+            }
+            neighbors += arr[i][j + 1];
+            neighbors += arr[i + 1][j];
+            neighbors += arr[i + 1][j + 1];
+            std::cout << neighbors;
+            std::cin.get();
+            neighbors = 0;
+        }
+    }
 }
--- main.cpp
+++ main.cpp
@@ -1,23 +1,35 @@
+#include "grid.h"
 #include <iostream>
 #include <string>
 #include <unistd.h>
-#include "grid.h"
+
+// void check(std::string arr[height][width]){
+//     for (int i = 0; i < height; i++){
+//         for (int j = 0; i < width; i++){
+
+//         }
+//     }
+// }
 
 int main(int argc, char const *argv[])
 {
-    std::string arr[height][width];
-    for (int i = 0; i < height; i++){
-        for (int j = 0; j <= width; j++){
-            arr[i][j] = "*";
+    srand(time(NULL));
+    int arr[height][width];
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j <= width; j++)
+        {
+            int r = rand() % 2;
+            arr[i][j] = r;
         }
     }
     draw(arr);
-    point(0, 1, arr);
+    std::cin.get();
+    evaluate(arr);
     draw(arr);
-    // for (int i = 0; i < 3; i++){
-    //     std::cout << ".";
-    //     fflush(stdout);
-    //     sleep(1);
-    // }
-    return 0;
+    // point(0, 1, arr);
+
+    // draw(arr);
+    // return 0;
 }

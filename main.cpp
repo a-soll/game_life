b4d36076730487c8061d5b61f29eca72d5783f06
Adam Solloway <asolloway@worcester.edu>
2020-10-24 19:22:02 -0400

    commit

--- main.cpp
+++ main.cpp
@@ -1,19 +1,41 @@
-#include "grid.h"
 #include <iostream>
 #include <string>
 #include <unistd.h>
+
+const int height = 10;
+const int width = 10;
 
 void point(int x, int y, int arr[width][height]) {
     arr[x][y] = 0;
 }
 
 void generate(int arr[][height]) {
+    FILE *field;
+    field = fopen("./field", "r");
+
     for (int i = 0; i < height; i++) {
-        for (int j = 0; j <= width; j++) {
-            int r = rand() % 2;
-            arr[i][j] = r;
+        for (int j = 0; j < width; j++) {
+            arr[i][j] = 0;
         }
     }
+
+    if (field) {
+        int num;
+        int x = 0;
+        int y = 0;
+        while ((num = getc(field)) != EOF) {
+            if (num == '1') {
+                arr[x][y] = 1;
+            }
+            if (num == '\n') {
+                x++;
+                y = 0;
+            } else {
+                y++;
+            }
+        }
+    }
+    fclose(field);
 }
 
 void draw(int arr[][width]) {
@@ -29,37 +51,56 @@ void draw(int arr[][width]) {
     }
 }
 
-void check_live(bool spot, int neighbors) {
-    if (spot) {
-        if (neighbors == 0) {
-            printf();
-        }
-    }
-}
+// void check_live(int next[][width], int x, int y, int* neighbors) {
+//     if (spot) {
+//         if (*neighbors < 2) {
+//             std::cout << "hey";
+//         }
+//     }
+// }
 
 void count_neighbors(int arr[][width], int x, int y, int *sum) {
-    int i = -1;
-    int j = -1;
+    int x_pos = -1;
+    int y_pos = -1;
+    int x_count = 1;
+    int y_count = 1;
 
-    if (x == 0 || x == height - 1) {
-        i = 0;
+    if (x == 0) {
+        x_pos = 0;
     }
-    for (int i = -1; i < 2; i++) {
-        for (int j = -1; j < 2; j++) {
-            *sum += arr[x][y];
+    if (y == 0) {
+        y_pos = 0;
+    }
+    if (x != width - 1) {
+        x_count = 2;
+    }
+    if (y != width - 1) {
+        y_count = 2;
+    }
+
+    for (int i = x_pos; i < x_count; i++) {
+        for (int j = y_pos; j < y_count; j++) {
+            *sum += arr[i + x_pos][y + y_pos];
         }
     }
+    sum -= arr[x][y];
 }
 
 int main(int argc, char const *argv[]) {
     srand(time(NULL));
     int arr[height][width];
-    int sum = 0;
+    int next[height][width];
+    int neighbors = 0;
+
     generate(arr);
     draw(arr);
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
-            count_neighbors(arr, i, j, &sum);
+            count_neighbors(arr, i, j, &neighbors);
+            if (neighbors > 0) {
+                std::cout << neighbors << std::endl;
+            }
+            neighbors = 0;
         }
     }
 }

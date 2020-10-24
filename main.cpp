3b2d4cb1481e54b7d94980f78494b2a3a22ea64f
Adam Solloway <asolloway@worcester.edu>
2020-10-24 17:10:14 -0400

    One file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,55 @@
 #include "grid.h"
 #include <iostream>
 #include <string>
+#include <unistd.h>
+
+void point(int x, int y, int arr[width][height]) {
+    arr[x][y] = 0;
+}
+
+void generate(int arr[][height]) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j <= width; j++) {
+            int r = rand() % 2;
+            arr[i][j] = r;
+        }
+    }
+}
+
+void draw(int arr[][width]) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j <= width; j++) {
+            if (j == width) {
+                std::cout << '\n';
+            } else {
+                std::cout << arr[i][j];
+                std::cout << ' ';
+            }
+        }
+    }
+}
+
+void check_live(bool spot, int neighbors) {
+    if (spot) {
+        if (neighbors == 0) {
+            printf();
+        }
+    }
+}
+
+void count_neighbors(int arr[][width], int x, int y, int *sum) {
+    int i = -1;
+    int j = -1;
+
+    if (x == 0 || x == height - 1) {
+        i = 0;
+    }
+    for (int i = -1; i < 2; i++) {
+        for (int j = -1; j < 2; j++) {
+            *sum += arr[x][y];
+        }
+    }
+}
 
 int main(int argc, char const *argv[]) {
     srand(time(NULL));

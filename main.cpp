302e3a568a02c286e5c030f6b5d0b4f7f6044c31
Adam Solloway <asolloway@worcester.edu>
2020-10-24 19:54:54 -0400

    Update

--- main.cpp
+++ main.cpp
@@ -59,48 +59,48 @@ void draw(int arr[][width]) {
 //     }
 // }
 
-void count_neighbors(int arr[][width], int x, int y, int *sum) {
+int count_neighbors(int arr[][width], int x, int y) {
     int x_pos = -1;
     int y_pos = -1;
-    int x_count = 1;
-    int y_count = 1;
-
-    if (x == 0) {
-        x_pos = 0;
-    }
-    if (y == 0) {
-        y_pos = 0;
-    }
-    if (x != width - 1) {
-        x_count = 2;
-    }
-    if (y != width - 1) {
-        y_count = 2;
-    }
+    int x_count = 2;
+    int y_count = 2;
+    int sum = 0;
 
     for (int i = x_pos; i < x_count; i++) {
         for (int j = y_pos; j < y_count; j++) {
-            *sum += arr[i + x_pos][y + y_pos];
+            int tmp_x = x + i;
+            int tmp_y = y + j;
+            if (tmp_x < 0) {
+                tmp_x = width - 1;
+            } else if (tmp_x >= width) {
+                tmp_x = 0;
+            }
+            if (tmp_y < 0) {
+                tmp_y = height - 1;
+            } else if (tmp_y >= height) {
+                tmp_y = 0;
+            }
+            sum += arr[tmp_x][tmp_y];
         }
     }
     sum -= arr[x][y];
+    return sum;
 }
 
 int main(int argc, char const *argv[]) {
     srand(time(NULL));
     int arr[height][width];
     int next[height][width];
-    int neighbors = 0;
+    int neighbors;
 
     generate(arr);
     draw(arr);
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
-            count_neighbors(arr, i, j, &neighbors);
+            neighbors = count_neighbors(arr, i, j);
             if (neighbors > 0) {
                 std::cout << neighbors << std::endl;
             }
-            neighbors = 0;
         }
     }
 }

f3d391e264c43d4a505f7fe13dd4f615d21343d9
Adam Solloway <asolloway@worcester.edu>
2020-10-24 16:37:50 -0400

    Update

--- main.cpp
+++ main.cpp
@@ -1,35 +1,16 @@
 #include "grid.h"
 #include <iostream>
 #include <string>
-#include <unistd.h>
 
-// void check(std::string arr[height][width]){
-//     for (int i = 0; i < height; i++){
-//         for (int j = 0; i < width; i++){
-
-//         }
-//     }
-// }
-
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     srand(time(NULL));
     int arr[height][width];
-
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j <= width; j++)
-        {
-            int r = rand() % 2;
-            arr[i][j] = r;
+    int sum = 0;
+    generate(arr);
+    draw(arr);
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            count_neighbors(arr, i, j, &sum);
         }
     }
-    draw(arr);
-    std::cin.get();
-    evaluate(arr);
-    draw(arr);
-    // point(0, 1, arr);
-
-    // draw(arr);
-    // return 0;
 }
